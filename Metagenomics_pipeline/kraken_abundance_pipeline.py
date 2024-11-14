9f455662d9b5d5baed82fab3496fc3711de2ea6a
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-14 16:37:00 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -13,16 +13,17 @@ import matplotlib.pyplot as plt
 def process_sample(forward, reverse, base_name, bowtie2_index, kraken_db, output_dir, threads, run_bowtie, use_precomputed_reports, contigs_file=None):
     try:
         
-        if contigs_file and not use_precomputed_reports:
-            print(f"Running Kraken2 on provided contigs file for sample {base_name}")
-            # Step: Run Kraken2 on the contigs file
-            kraken_report = run_kraken2(forward, None, base_name, kraken_db, output_dir, threads)
-            #return kraken_report  # Return the Kraken report generated from contigs file
-        elif use_precomputed_reports and contigs_file:
+        if contigs_file:
+            if use_precomputed_reports:
                 kraken_report = os.path.join(output_dir, f"{base_name}_report.txt")
+                 
+            else:
+               print(f"Running Kraken2 on provided contigs file for sample {base_name}")
+            # Step: Run Kraken2 on the contigs file
+               kraken_report = run_kraken2(forward, None, base_name, kraken_db, output_dir, threads)
             
         
-        elif not use_precomputed_reports and not contigs_file:
+        if use_precomputed_reports and not contigs_file:
             # Step 1: Run Trimmomatic
             trimmed_forward, trimmed_reverse = run_trimmomatic(forward, reverse, base_name, output_dir, threads)
 
