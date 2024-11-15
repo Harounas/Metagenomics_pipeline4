94bdcd86e54063f63a523230771687185fa38dbf
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-15 12:03:46 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -12,12 +12,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 def process_sample(forward, reverse, base_name, bowtie2_index, kraken_db, output_dir, threads, run_bowtie, use_precomputed_reports, contigs_file=None):
     try:
-        if contigs_file:
+        if contigs_file and not use_precomputed_reports :
             print(f"Skipping Trimmomatic, Bowtie2, and Kraken2 steps as --contigs_file is provided for sample {base_name}")
             kraken_report = run_kraken2(contigs_file, None, base_name, kraken_db, output_dir, threads)
             return kraken_report  # If contigs are provided, return the file directly as output
         
-        if not use_precomputed_reports:
+        if not use_precomputed_reports and not contigs_file :
             # Step 1: Run Trimmomatic
             trimmed_forward, trimmed_reverse = run_trimmomatic(forward, reverse, base_name, output_dir, threads)
 
