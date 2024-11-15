87a9a5d5f3f2a9cd30b3ad73d0916fc22e709de4
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-15 15:32:50 +0100

    Update run_kr_abundance2.py

--- scripts/run_kr_abundance2.py
+++ scripts/run_kr_abundance2.py
@@ -100,7 +100,7 @@ def main():
         run_bowtie = not args.no_bowtie2 and args.bowtie2_index is not None
 
      """
-        for forward in glob.glob(os.path.join(args.input_dir, "*_R1*.fastq*")):
+    for forward in glob.glob(os.path.join(args.input_dir, "*_R1*.fastq*")):
             base_name = os.path.basename(forward)
             base_name = base_name.replace("_R1_001.fastq.gz", "").replace("_R1_001.fastq", "")
             base_name = base_name.replace("_R1.fastq.gz", "").replace("_R1.fastq", "")
