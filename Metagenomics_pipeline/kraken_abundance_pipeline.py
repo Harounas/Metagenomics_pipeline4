d554b763156f2ff2584088d42c0c5db0e7cc71ec
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2024-11-26 10:54:39 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -77,7 +77,7 @@ def generate_sample_ids_csv(kraken_dir):
         print(f"Error generating sample_ids.csv: {e}")
         return None
         
-def aggregate_kraken_results(kraken_dir, metadata_file=None, sample_id_df=None, read_count=1,max_read_count=1000000000000):
+def aggregate_kraken_results(kraken_dir, metadata_file=None, sample_id_df=None, read_count=1,max_read_count=1000000000000000000000000000):
     """
     Aggregates Kraken results, merging metadata or using sample IDs if metadata is not provided.
 
@@ -131,7 +131,7 @@ def aggregate_kraken_results(kraken_dir, metadata_file=None, sample_id_df=None, 
                                 aggregated_results[sampleandtaxonid] = {
                                     'Perc_frag_cover': perc_frag_cover,
                                     'Nr_frag_cover': nr_frag_cover,
-                                    'nr_frag_direct_at_taxon': nr_frag_direct_at_taxon,
+                                    'Nr_frag_direct_at_taxon': nr_frag_direct_at_taxon,
                                     'Rank_code': rank_code,
                                     'NCBI_ID': ncbi_ID,
                                     'Scientific_name': scientific_name,
@@ -143,7 +143,7 @@ def aggregate_kraken_results(kraken_dir, metadata_file=None, sample_id_df=None, 
         merged_tsv_path = os.path.join(kraken_dir, "merged_kraken.tsv")
         with open(merged_tsv_path, 'w') as f:
             # Write headers dynamically
-            headers = ['Perc_frag_cover', 'Nr_frag_cover', 'nr_frag_direct_at_taxon', 'Rank_code', 'NCBI_ID', 'Scientific_name', 'SampleID'] + metadata.columns[1:].tolist()
+            headers = ['Perc_frag_cover', 'Nr_frag_cover', 'Nr_frag_direct_at_taxon', 'Rank_code', 'NCBI_ID', 'Scientific_name', 'SampleID'] + metadata.columns[1:].tolist()
             f.write("\t".join(headers) + "\n")
             for sampleandtaxonid, data in aggregated_results.items():
                 f.write("\t".join(str(data[col]) for col in headers) + "\n")
@@ -187,7 +187,7 @@ def generate_abundance_plots(merged_tsv_path, top_N,col_filter,pat_to_keep):
             categorical_cols.remove(focus)
 
             for col in categorical_cols:
-                grouped_sum = df_focus.groupby([focus, col])['nr_frag_direct_at_taxon'].mean().reset_index()
+                grouped_sum = df_focus.groupby([focus, col])['Nr_frag_direct_at_taxon'].mean().reset_index()
                 # Create a color mapping based on unique values in the 'focus' column
                 #colordict = dict(zip(grouped_sum[focus].unique(), distinctipy.get_colors(len(grouped_sum[focus].unique()))))
                 colordict = defaultdict(int)
@@ -251,7 +251,7 @@ def generate_abundance_plots(merged_tsv_path, top_N,col_filter,pat_to_keep):
                 fig = px.bar(
                     grouped_sum,
                     x=col,
-                    y='nr_frag_direct_at_taxon',
+                    y='Nr_frag_direct_at_taxon',
                     color=focus,
                     color_discrete_map=colordict,
                     title=f"{plot_title} Abundance by {col}"
