14d0ed9ce125a40b50ec6bab21e3305ea343cb9c
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2024-12-03 14:13:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Metagenomics_pipeline3",
+    name="Metagenomics_pipeline4",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -11,7 +11,7 @@ setup(
     entry_points={
         "console_scripts": [
            # "run_kr_abundance=Metagenomics_pipeline.scripts.run_kr_abundance:main",
-             "run_kr_abundance2=scripts.run_kr_abundance2:main",
+             "run_kr_abundance4=scripts.run_kr_abundance4:main",
         ],
     },
     author="Harouna",
