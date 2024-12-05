632fdc2de3a68d8b798bc87a8b07b5ffe7515ca8
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2024-12-05 15:07:15 +0100

    Update ref_based_assembly.py

--- Metagenomics_pipeline/ref_based_assembly.py
+++ Metagenomics_pipeline/ref_based_assembly.py
@@ -123,7 +123,7 @@ def ref_based(df, run_bowtie, input_dir):
         # Add a new column and conditionally set values
         dftax['Ref_len'] = dftax.apply(lambda row: ref_genome_len if row['NCBI_ID'] == tax and row['SampleID'] == sample else row['Ref_len'],axis=1)
         dftax['Consensus_len'] = dftax.apply(lambda row: consensus_genome_len if row['NCBI_ID'] == tax and row['SampleID'] == sample else row['Consensus_len'], axis=1)
-        dftax['Completness%'] = dftax.apply(lambda row:f"{consensus_genome_len / ref_genome_len:.2f}" if row['NCBI_ID'] == tax and row['SampleID'] == sample else row['Completness%'], axis=1) 
+        dftax['Completness%'] = dftax.apply(lambda row:f"{consensus_genome_len *100/ ref_genome_len:.2f}" if row['NCBI_ID'] == tax and row['SampleID'] == sample else row['Completness%'], axis=1) 
 
 merged_df = pd.concat(dfs, ignore_index=True)
 merged_df.to_csv("Output-summary.csv", index=False)
