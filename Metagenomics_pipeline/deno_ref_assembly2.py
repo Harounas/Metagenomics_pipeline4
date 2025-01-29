db3672a83eeeb583a7642bf8c13fc9ae978b6044
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2025-01-29 09:19:32 +0100

    Update deno_ref_assembly2.py

--- Metagenomics_pipeline/deno_ref_assembly2.py
+++ Metagenomics_pipeline/deno_ref_assembly2.py
@@ -232,7 +232,10 @@ def deno_ref_based(df, input_dir, output_dir, run_bowtie):
             reference_list = split_fasta(input_files, f"{output_dir}/{sample}_{scientific_name}")
             fasta_file=get_best_reference(sample_r1, sample_r2, reference_list)
             cmd = f"grep '^>' {fasta_file} | cut -d ' ' -f1 | sed 's/^>//'"
-            acc = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+            acc = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+
+            # To get the output as a list of sequence headers:
+            acc_ids = acc.stdout.strip().split("\n")
             # Perform further processing on the assembly
             #rag_file = os.path.join(tax_dir, "ragtag.scaffold.fasta")
             if os.path.exists(rag_file):
@@ -282,7 +285,7 @@ def deno_ref_based(df, input_dir, output_dir, run_bowtie):
                 print(type(ref_len), type(consensus_len), type(completeness), type(sequence))
 
                 #logging.info(f"sequence {sequence}.")
-                dftax.loc[dftax['SampleID'] == sample, ['Ref_len','Contig_len', 'Consensus_len', 'Completeness(%)', 'sequence','Accession_number']] = [ref_len, contig_len,consensus_len, completeness, str(sequence),acc]
+                dftax.loc[dftax['SampleID'] == sample, ['Ref_len','Contig_len', 'Consensus_len', 'Completeness(%)', 'Accession_number','sequence']] = [ref_len, contig_len,consensus_len, completeness, acc_ids,str(sequence)]
             except Exception as e:
                 logging.error(f"Error processing sample {sample}: {e}")
         
